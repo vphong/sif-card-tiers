d7f9a31a533a8e1ccc4d29cdfcf9f3afdb86a0ee
Vanna Phong <vphong@ucsd.edu>
2016-08-26 10:37:27 -0700

    initialize raw card data

--- javascripts/get_card_data.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import json
-
-url = "https://schoolido.lu/api/cards/?&page_size=10&ordering=-id&rarity=SR,SSR,UR"
-
-response = urllib.request.urlopen(url)
-data = (response.read().decode("utf-8").encode('utf-8'))
-#data = eval(data).stringify(eval(data))
-print(data)
