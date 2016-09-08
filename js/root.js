24d074414383f5f50e564482e2d197214ee4e243
Vanna Phong <vphong@ucsd.edu>
2016-09-08 12:54:46 -0700

    search info, more typeahead terms

--- js/root.js
+++ js/root.js
@@ -1,4 +1,15 @@
-app.constant('Typeahead', ['Nishikino Maki', 'Koizumi Hanayo', 'Hoshizora Rin', 'Minami Kotori', 'Sonoda Umi', 'Kousaka Honoka', 'Ayase Eli', 'Toujou Nozomi', 'Yazawa Nico']);
+app.constant('Typeahead', [
+    'Nishikino Maki', 'Koizumi Hanayo', 'Hoshizora Rin',
+    'Minami Kotori', 'Sonoda Umi', 'Kousaka Honoka',
+    'Ayase Eli', 'Toujou Nozomi', 'Yazawa Nico',
+    "Ohara Mari", "Kurosawa Dia", "Matsuura Kanan",
+    "Takami Chika", "Sakurauchi Riko", "Watanabe You",
+    "Kunikida Hanamaru", "Kurosawa Ruby", "Tsushima Yoshiko",
+    "lily white", "Printemps", "BiBi",
+    "AZALEA", "CYaRon!", "Guilty Kiss",
+    "Pure", "Cool", "Smile", "Muse", "Aqours",
+    "Score Up", "Perfect Lock", "Healer"
+]);
 
 app.constant('InitFilters', {
     server: 'jp',
