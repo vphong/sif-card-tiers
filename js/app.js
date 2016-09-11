91fe4b1e56967b67a0e3c8a91e04d57d5e876aa6
Vanna Phong <vphong@ucsd.edu>
2016-09-11 01:07:55 -0700

    affix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('tierList', ['ui.bootstrap', 'ui.router.tabs',
     'bsLoadingOverlay', 'bsLoadingOverlayHttpInterceptor', 'fsm',
-    'ui.router', 'LocalStorageModule', 
+    'ui.router', 'LocalStorageModule', 'mgcrea.ngStrap' 
 ]);
 
 app.factory('allHttpInterceptor', function(bsLoadingOverlayHttpInterceptorFactoryFactory) {
